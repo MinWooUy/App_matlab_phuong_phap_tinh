1caa61f50f6f04f6248a59b86551fcdb50a8ab25
Bui Gia Huy <bhuy2652005@gmail.com>
2025-12-31 12:49:28 +0700

    Update Gauss and GaussJordan

---
+++ Gauss_Jordan_test.m
@@ -0,0 +1,59 @@
+clear; clc;
+A = [10+30,-10,-30;-10,10+15+5,-5;-30,-5,5+30+30];
+b = [10; 0; 0];
+
+% ------------- Cơ sở lý thuyết -------------
+Ab = [A, b]; % Ghép hai ma trận
+%% Bước 1: Quá trình thuận
+% 1.1: Chuẩn hóa hàng 1 ma trận
+Ab(1,:) = Ab(1,:) ./ Ab(1,1);
+% 1.2: Khử x1 ở hàng 2 & 3
+Ab(2,:) = Ab(2,:) - Ab(2,1)*Ab(1,:);
+Ab(3,:) = Ab(3,:) - Ab(3,1)*Ab(1,:);
+% 1.3: Chuẩn hóa hàng 2 ma trận
+Ab(2,:) = Ab(2,:) ./ Ab(2,2);
+% 1.4: Khử x2 ở hàng 1 & 3
+Ab(1,:) = Ab(1,:) - Ab(1,2)*Ab(2,:);
+Ab(3,:) = Ab(3,:) - Ab(3,2)*Ab(2,:);
+% 1.5: Chuẩn hóa hàng 3 ma trận
+Ab(3,:) = Ab(3,:) ./ Ab(3,3);
+% 1.6: Khử x3 ở hàng 1 & 2
+Ab(1,:) = Ab(1,:) - Ab(1,3)*Ab(3,:);
+Ab(2,:) = Ab(2,:) - Ab(2,3)*Ab(3,:);
+
+%% Bước 2: Quá trình nghịch
+% 2.1: Tìm x3
+X(3,1) = Ab(3,4) ./ Ab(3,3);
+% 2.2: Tìm x2
+X(2,1) = Ab(2,4) ./ Ab(2,2);
+% 2.3: Tìm x1
+X(1,1) = Ab(1,4) ./ Ab(1,1);
+
+disp('Giải hệ phương trình:');
+disp(X);
+
+clear; clc;
+A = [10+30,-10,-30;-10,10+15+5,-5;-30,-5,5+30+30];
+b = [10; 0; 0];
+Ab = [A, b];
+% ------------- Build Hàm -------------
+%% Quá trình thuận
+[m, n] = size(Ab);
+for k = 1:m
+    Ab(k, :) = Ab(k,:) ./ Ab(k,k);
+    for l = 1:m
+        if l ~= k
+            Ab(l,:) = Ab(l,:) - Ab(l,k).*Ab(k,:);
+        end
+    end
+end
+
+disp(Ab);
+
+%% Quá trình nghịch
+X = size(m, 1);
+X(m,1) = Ab(m,n)/Ab(m,m);
+for k = (m-1):-1:1
+    X(k) = (Ab(k, n) - Ab(k,k+1:m)*X(k+1:m)) / Ab(k,k);
+end
+disp(X);
